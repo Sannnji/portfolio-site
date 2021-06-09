34aa717a7a633053ce7710cbd31677c4c81d16ce
Yams <57771801+Sannnji@users.noreply.github.com>
2021-06-09 16:14:04 -0400

    added custom framer chakraui component

--- src/components/cards/HeaderCard.tsx
+++ src/components/cards/HeaderCard.tsx
@@ -1,34 +1,31 @@
 import { Box, Image, Flex } from "@chakra-ui/react";
-import { motion, animate } from "framer-motion";
 
 import IntroText from "../framer/IntroText";
+import { MotionBox } from "../framer/MotionBox";
 
 import "./HeaderCard.css";
 import cityScape from "../../images/cityScape.png";
 import rochLogo from "../../images/rochLogo.png";
 
-function HeaderCard() {
+export default function HeaderCard() {
   return (
-    <Box className="test" pt={8} borderRadius="lg" boxShadow="dark-lg">
-
-        <Flex mb={4} mx={4} textAlign="left" align="center" justify="center">
-          {/* <motion.img
-          src={rochLogo}
-          width="85px"
-          style={{ marginRight: 25 }}
-          animate={{ y: 10 }}
-          transition={{ repeat: Infinity, duration: 2 }}
-        /> */}
-          <Image mr={10} width={{ base: "85px", md: "150px" }} src={rochLogo} />
-          <IntroText />
-        </Flex>
-        <Flex>
-          <Image src={cityScape} borderBottomLeftRadius="md" width="50%" />
-          <Image src={cityScape} borderBottomRightRadius="md" width="50%" />
-        </Flex>
-
-    </Box>
+    <MotionBox
+      className="test"
+      pt={8}
+      borderRadius="lg"
+      boxShadow="dark-lg"
+      initial={{ scaleX: 0.5 }}
+      animate={{ scaleX: 1 }}
+      transition={{ duration: ".5", ease: "easeIn" }}
+    >
+      <Flex mb={4} mx={4} textAlign="left" align="center" justify="center">
+        <Image mr={10} width={{ base: "85px", md: "150px" }} src={rochLogo} />
+        <IntroText />
+      </Flex>
+      <Flex>
+        <Image src={cityScape} borderBottomLeftRadius="md" width="50%" />
+        <Image src={cityScape} borderBottomRightRadius="md" width="50%" />
+      </Flex>
+    </MotionBox>
   );
 }
-
-export default HeaderCard;
