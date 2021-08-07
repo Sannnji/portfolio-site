b6202403b3dd41dac7c8416d270cc83a194dca96
James Ji <jamesji.penfield@protonmail.com>
2021-08-07 13:55:57 -0400

    -overflow hidden

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <Box mx={{ base: "5%", xl: "13%" }} overflow="hidden">
+    <Box mx={{ base: "5%", xl: "13%" }} >
       {children}
     </Box>
   );
