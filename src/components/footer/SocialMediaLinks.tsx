9b19693b7ca35a58d687dd70f1f4b9a7ffab791d
James Ji <jamesji.penfield@protonmail.com>
2021-08-11 14:25:41 -0400

    added missing social links in footer

--- src/components/footer/SocialMediaLinks.tsx
+++ src/components/footer/SocialMediaLinks.tsx
@@ -4,8 +4,7 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 export const SocialMediaLinks = (props: ButtonGroupProps) => (
   <ButtonGroup variant="ghost" color="gray.600" {...props}>
-    <IconButton as="a" href="#" aria-label="LinkedIn" icon={<FaLinkedin fontSize="20px" />} />
-    <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="20px" />} />
-    <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="20px" />} />
+    <IconButton as="a" href="https://www.linkedin.com/in/jamesji98/" aria-label="LinkedIn" icon={<FaLinkedin fontSize="20px" />} />
+    <IconButton as="a" href="https://github.com/Sannnji" aria-label="GitHub" icon={<FaGithub fontSize="20px" />} />
   </ButtonGroup>
 )
