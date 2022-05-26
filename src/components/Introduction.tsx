f9958f1fba96d03bb2b658a65a26f7949ab0aacc
Sannnji <jamesji.penfield@protonmail.com>
2022-05-26 16:12:21 -0400

    edited into paragraph

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,17 +6,17 @@ export const Introduction = () => {
       <Box width="75%">
         <Heading mb={4}>Quick About Me</Heading>
         <Text fontWeight="semibold">
-          Due to how the accessibility of education and information has changed
-          and grown. I dropped out of college in the winter of 2019 to pursue my
-          developer career independently. Since then, I've completed a udemy
-          course on full stack web development, a full stack web mobile bootcamp
-          offered by NuCamp, and spent time exploring Java, Flutter and Python.
+          Due to COVID-19 and wanting to pursue a career in tech independently,
+          I dropped out of community college back in 2020. Since then, I've
+          built many projects, completed a WozU .NET bootcamp, a full stack MERN
+          bootcamp offered by NuCamp, and spent time exploring various
+          languages.
         </Text>
         <br />
         <Text fontWeight="semibold">
           When I'm not coding, you can find me gaming / streaming, drawing,
           cooking, finding ways to improve myself, and watching the Harry Potter
-          series for the millionth time
+          series for the millionth time!
         </Text>
       </Box>
     </Flex>
