ba028a4d2fbd5b359e452b80f8e388d3b63ace3d
Sannnji <jamesji.penfield@protonmail.com>
2022-05-26 15:58:50 -0400

    added smooth scrolling behavior

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,6 +4,11 @@ const theme = extendTheme({
   initialColorMode: "light",
   colors: { primary: "#E8E8E8" },
   styles: {
+    global: {
+      html: {
+        scrollBehavior: "smooth"
+      }
+    },
     backgroundColor: "#404040",
   },
 });
